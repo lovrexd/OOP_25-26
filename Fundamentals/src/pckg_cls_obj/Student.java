9637ed1818aee9d148ab2d66abc271276e6266a1
lovre <lovre.ljoka@gmail.com>
2026-03-15 21:50:02 +0100

    3 koda

---
+++ Fundamentals/src/pckg_cls_obj/Student.java
@@ -0,0 +1,47 @@
+package pckg_cls_obj;
+
+public class Student {
+
+    private final String name;
+    private final String surname;
+    private int id;
+    private static int conterID = 100;
+
+    public Student(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Student(String name, String surname, int id) {
+        this(name, surname);
+        this.id = id;
+    }
+
+//    public Student(String name, String surname) {
+//        this.id = counterID;
+//        counterID++;
+//        this.name = name;
+//        this.surname = surname;
+//    }
+
+    public void subscribeToStudentService() {
+        System.out.println("Student's id: " + id);
+        System.out.println(name + " " + surname + " is subscribed to student service.");
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    public Hlace operiMe(Hlace hlace) {
+        hlace.setCiste(true);
+        System.out.println(this + " je oprao hlače!");
+
+        return hlace;
+    }
+}
