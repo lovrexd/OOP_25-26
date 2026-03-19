685b55ac080667429eed1e34a07b297031c8e7e8
lovre ljoka <lovrelana2@gmail.com>
2026-03-19 22:11:31 +0100

    Delete Fundamentals/src directory

--- Fundamentals/src/pckg_static_cls/AUX_CLS.java
+++
@@ -1,9 +0,0 @@
-package pckg_static_cls;
-
-public class AUX_CLS {
-    static void printSomeMSG(String MSG) {
-
-
-
-    }
-}
--- Fundamentals/src/pckg_uml_abs/TestClient.java
+++
@@ -1,9 +0,0 @@
-package pckg_uml_abs;
-
-import pckg_uml.SportCar;
-
-public class TestClient {
-    SportGPMotor mot1 = new SportGPMotor("Ducati", "H1000");
-    SportCar spc = new SportCar("Mercedes", "c63 amg");
-    mot1.startVeh();
-}
